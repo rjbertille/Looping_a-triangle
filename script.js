f586f8431ca6af7aa889f2bba53d130c8ba5849d
rjbertille <rjbertille@gmail.com>
2018-07-30 13:06:45 +0200

    js

---
+++ script.js
@@ -0,0 +1,5 @@
+//write a loop makes seven calls to console.log to output the following traingle
+//Be useful to know that you can find the length of a string by writing .length after it
+for(let i = "#"; i.length < 8; i += "#") {
+	console.log(i);
+}
